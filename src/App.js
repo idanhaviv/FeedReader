8244043c603a7cb2d9f54bff25a8f54648e481a8
idan haviv <idan.haviv@gmail.com>
2018-08-16 13:27:12 +0300

    present feed text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+
+const apiService = axios.create({
+  baseURL: "http://localhost:8080/api"
+});
+
+// apiService
+//   .get()
+//   .then(res => console.log("res: ", res.data))
+//   .catch(e => console.log("e: ", e));
+
+// const getFeed = async path => {
+//   const res = await apiService.get();
+//   if (!res || !res.data) {
+//     throw new Error("unexpected result for fetch question");
+//   }
+//   return res.data;
+// };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: "" };
+  }
+
+  componentDidMount() {
+    apiService
+      .get()
+      .then(res => this.setState({ data: res.data }))
+      .catch(e => console.log("e: ", e));
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +41,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
