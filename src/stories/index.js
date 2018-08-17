98999a0ff760792439f17dc16361dbf2a777720e
idan haviv <idan.haviv@gmail.com>
2018-08-17 15:29:25 +0300

    feedItem component

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from "@storybook/react/demo";
+import FeedItem from "../components/FeedItem";
+import { CardMedia } from "@material-ui/core";
+import imageFile from "./static/download.jpeg";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
   ));
+
+storiesOf("FeedItem", module).add("component", () => <FeedItem />);
+storiesOf("CardMedia", module).add("component", () => (
+  <CardMedia img={imageFile} />
+));
+
+storiesOf("<img />", module).add("with an image", () => (
+  <img src={imageFile} />
+));
